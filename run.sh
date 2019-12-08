863c0df999f36effa0a773b8ef9343fada732101
himkt <himkt@cookpad.com>
2019-12-08 11:05:50 +0900

    Fix typo in section name

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 cd src
-python main.py -config ../config/config -section test_run > ./log.txt
+python main.py -config ../config/config -section test-run > ./log.txt
 cat ./log.txt | grep 'Meta-Test\|Meta-Valid' | python process_results.py
